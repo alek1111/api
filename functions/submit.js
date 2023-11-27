c67e0aff1b1fcfd6342e7b034d7a18f1765454a9
agvozdev@winstonbrands.com <agvozdev@collectionsetc.com>
2023-11-27 13:32:02 -0600

    Update submit.js

--- functions/submit.js
+++ functions/submit.js
@@ -1,17 +1,18 @@
 
 export async function onRequestGet(context) {
   
+  const fetchUrl = "https://lusearchapi-na.hawksearch.com/sites/collectionsetcrwdv2/";
+  
   try {
     let url = await context.request.url;
 
     let paramString = url.split('?')[1];
     let queryString = new URLSearchParams(paramString);
 
-    return new Response(paramString, {
-      headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-      },
+    return fetch(fetchUrl.concat('?', paramString), {
+      method: "GET",
   });
+  
   } catch (err) {
     return new Response('Error parsing JSON content', { status: 400 });
   }
