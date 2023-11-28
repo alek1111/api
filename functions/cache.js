a78846a6df107975472e1e7d79e85a23418b0545
agvozdev@winstonbrands.com <agvozdev@collectionsetc.com>
2023-11-28 13:16:28 -0600

    Create cache.js

---
+++ functions/cache.js
@@ -0,0 +1,25 @@
+//new deploy - 1
+
+export async function onRequestGet(context) {
+  
+  const fetchUrl = "https://lusearchapi-na.hawksearch.com/sites/collectionsetcrwdv2/";
+  const testUrl = "https://lusearchapi-na.hawksearch.com/sites/collectionsetcrwdv2/";
+  
+  try {
+    let url = await context.request.url;
+
+    let paramString = url.split('?')[1];
+	
+	console.log(fetchUrl.concat('?', paramString));
+	
+    return fetch(testUrl.concat('?', paramString), {
+      method: "GET",
+	  cf: { cacheTtl: 1200,
+			cacheEverything: true,
+			cacheKey: fetchUrl.concat('?', paramString) },
+  });
+  
+  } catch (err) {
+    return new Response('Error parsing JSON content', { status: 400 });
+  }
+}
