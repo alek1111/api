ef4ff9d7be33f724782880006fe4d21a8b4aa081
agvozdev@winstonbrands.com <agvozdev@collectionsetc.com>
2023-11-27 22:46:08 -0600

    Update submit.js

--- functions/submit.js
+++ functions/submit.js
@@ -2,13 +2,20 @@
 export async function onRequestGet(context) {
   
   const fetchUrl = "https://lusearchapi-na.hawksearch.com/sites/collectionsetcrwdv2/";
+  const cache = caches.default;
   
   try {
     let url = await context.request.url;
 
     let paramString = url.split('?')[1];
     let queryString = new URLSearchParams(paramString);
-
+	
+	let response = cache.match(fetchUrl.concat('?', paramString));
+	
+	if (!response) {
+		return new Response('Error retreiving cache', { status: 400 })
+	};
+	
     return fetch(fetchUrl.concat('?', paramString), {
       method: "GET",
 	  cf: { cacheKey: fetchUrl.concat('?', paramString) },
