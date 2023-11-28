13d94062a6e22d85278c26eb1eee6fccbaf69e1f
agvozdev@winstonbrands.com <agvozdev@collectionsetc.com>
2023-11-28 16:07:34 -0600

    Update submit.js

--- functions/submit.js
+++ functions/submit.js
@@ -2,27 +2,39 @@
 
 export async function onRequestGet(context) {
   
-  const fetchUrl = "https://lusearchapi-na.hawksearch.com/sites/collectionsetcrwdv2/";
-  
-  try {
-    let url = await context.request.url;
+    const cacheUrl = new URL("https://lusearchapi-na.hawksearch.com/sites/collectionsetcrwdv2/");
+    
+	let url = await context.request.url;
 
     let paramString = url.split('?')[1];
 	
-    let response = new Response(fetch(fetchUrl.concat('?', paramString), {
-      method: "GET",
-	  cf: { cacheTtl: 1200,
-			cacheEverything: true,
-			cacheKey: fetchUrl.concat('?', paramString) },
-  }));
-  
-  console.log(response.header);
-  console.log(response.url);
-  console.log(response.body);
-  
-  return response.header.text;
-  
-  } catch (err) {
-    return new Response('Error parsing JSON content', { status: 400 });
-  }
+    // Construct the cache key from the cache URL
+    const cacheKey = new Request(cacheUrl.toString().concat('?', paramString));
+    const cache = caches.default;
+
+    // Check whether the value is already available in the cache
+    // if not, you will need to fetch it from origin, and store it in the cache
+    let response = await cache.match(cacheKey);
+
+    if (!response) {
+      console.log(
+        `Response for request url: ${request.url} not present in cache. Fetching and caching request.`
+      );
+      // If not in cache, get it from origin
+      response = await fetch(request);
+
+      // Must use Response constructor to inherit all of response's fields
+      response = new Response(response.body, response);
+
+      // Cache API respects Cache-Control headers. Setting s-max-age to 10
+      // will limit the response to be in cache for 10 seconds max
+
+      // Any changes made to the response here will be reflected in the cached value
+      response.headers.append("Cache-Control", "s-maxage=10");
+
+      ctx.waitUntil(cache.put(cacheKey, response.clone()));
+    } else {
+      console.log(`Cache hit for: ${request.url}.`);
+    }
+    return response;
 }
