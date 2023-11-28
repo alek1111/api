8e5b6b0a9a74c0fe203ddcdfbca5d7439964303b
agvozdev@winstonbrands.com <agvozdev@collectionsetc.com>
2023-11-28 15:29:33 -0600

    Update submit.js

--- functions/submit.js
+++ functions/submit.js
@@ -20,7 +20,7 @@ export async function onRequestGet(context) {
   console.log(response.url);
   console.log(response.body);
   
-  return response;
+  return response.header;
   
   } catch (err) {
     return new Response('Error parsing JSON content', { status: 400 });
