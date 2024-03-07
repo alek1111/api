7d9c3f43b103f1efb966443bb386f897106e807d
agvozdev@winstonbrands.com <agvozdev@collectionsetc.com>
2024-03-07 11:31:26 -0600

    Update cache.js

--- functions/cache.js
+++ functions/cache.js
@@ -6,21 +6,24 @@ export async function onRequestGet(context) {
   
   try {
     let url = await context.request.url;
-
-    let paramString = url.split('?')[1];
 	
-    return fetch(fetchUrl.concat('?', paramString), {
-      method: "GET",
-	  cf: {
-        // Always cache this fetch regardless of content type
-        // for a max of 300 seconds before revalidating the resource
-        cacheTtl: 300,
-        cacheEverything: true,
-        //Enterprise only feature, see Cache API for other plans
-        cacheKey: url,
-      },
-  });
-  
+	if (url.includes("NTT=") || url.includes("LPURL=")) 
+		{
+
+		let paramString = url.split('?')[1];
+	
+		return fetch(fetchUrl.concat('?', paramString), {
+		method: "GET",
+		cf: {
+			// Always cache this fetch regardless of content type
+			// for a max of 300 seconds before revalidating the resource
+			cacheTtl: 300,
+			cacheEverything: true,
+			//Enterprise only feature, see Cache API for other plans
+			cacheKey: url,
+				},
+			});
+		}
   } catch (err) {
     return new Response('Error parsing JSON content', { status: 400 });
   }
