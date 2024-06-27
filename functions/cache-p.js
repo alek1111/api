e533b6201725b9a3e034224bbe0eff7a68c94f02
agvozdev@winstonbrands.com <agvozdev@collectionsetc.com>
2024-06-27 14:04:37 -0500

    Create cache-p.js
    new file cache-p

---
+++ functions/cache-p.js
@@ -0,0 +1,35 @@
+//new deploy - 1
+
+export async function onRequestGet(context) {
+  
+  const fetchUrl = "https://lusearchapi-na.hawksearch.com/sites/collectionsetcrwdv2/";
+  
+  try {
+    let url = await context.request.url;
+
+	let paramString = url.split('?')[1];
+	
+	if (paramString.includes('ntt=') || paramString.includes('lpurl') || paramString.includes('GetSuggestions')) 
+		{
+			const today = new Date();
+			const tomorrow = new Date(today.getFullYear(), today.getMonth(), today.getDate() + 1);
+			const seconds = Math.floor((tomorrow.getTime() + 18000000 - today.getTime()) / 1000);
+			console.log(seconds);
+
+			let response = await fetch(fetchUrl.concat('?', paramString), {
+			method: "GET",
+			cf: {
+				cacheTtl: seconds,
+				cacheEverything: true,
+				cacheKey: fetchUrl.concat('?', paramString),
+					},
+				});
+				let headersObject = Object.fromEntries(response.headers);
+				let responseHeaders = JSON.stringify(headersObject, null, 2);
+				console.log(`Response headers: ${responseHeaders}`);  
+				return response;
+		}
+  } catch (err) {
+    return new Response('Error parsing JSON content', { status: 400 });
+  }
+}
